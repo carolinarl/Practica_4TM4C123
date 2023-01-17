14b0b1b68183a086c729a673795e1a52c5e03c93
carolinarl <carito.03ags@hotmail.com>
2023-01-17 17:12:39 -0600

    commit

---
+++ lib/ADC.h
@@ -0,0 +1,16 @@
+/*
+ * ADC.h
+ *
+ *  Created on: 04/11/2019
+ *      Author: ravenelco
+ */
+
+#ifndef ADC_ADC_H_
+#define ADC_ADC_H_
+
+extern void Configura_Reg_ADC0(void);
+extern void ADC0_InSeq3(uint16_t *Result);
+extern void ADC0_InSeq2(uint16_t *Result);
+extern void ADC1_InSeq0(uint16_t *Result);
+
+#endif /* ADC_ADC_H_ */
